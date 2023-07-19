7d8f6bf2aaa060f4ab1ff48a79513ec9a6697a46
cbrognara <csbrognara@gmail.com>
2023-07-19 14:57:45 -0300

    inital

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const http = require('http');
+const url = require('url');
+
+const routes = require('./routes')
+
+const server = http.createServer((request, response) => {
+    const parsedUrl = url.parse(request.url, true); 
+
+    console.log(parsedUrl)
+
+    console.log(`Request method: ${request.method} | Endpoint: ${parsedUrl.pathname}`);
+
+    const route = routes.find((routeObj) => (
+        routeObj.endpoint === parsedUrl.pathname && routeObj.method === request.method
+    ))
+
+    if (route) {
+        request.query = parsedUrl.query;
+        route.handler(request, response);
+    } else {
+        response.writeHead(404, { 'Content-Type': 'text/html' });
+        response.end(`Cannot ${request.method} ${parsedUrl.pathname}`);
+    }
+});
+
+server.listen(3000, () => console.log('server started at http://localhost:3000'));
---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Alice'
+    },
+    {
+        id: 2,
+        name: 'Bernardo'
+    },
+    {
+        id: 3,
+        name: 'Camila'
+    }
+];
