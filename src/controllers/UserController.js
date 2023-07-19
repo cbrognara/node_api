75f4db96bdd27eab0e2c038f49b78f3d4f93e77a
cbrognara <csbrognara@gmail.com>
2023-07-19 15:39:58 -0300

    feat: post and send body

--- src/controllers/UserController.js
+++ src/controllers/UserController.js
@@ -23,5 +23,27 @@ module.exports = {
             return response.send(400, { error: 'User not found' });
         }
         response.send(200, user);  
-    }
+    },
+
+    createUser(request, response) {
+        let body = ''
+        request.on('data', (chunk) => {
+            body += chunk;
+        });
+
+        request.on('end', () => {
+            body = Json.parse(body);
+
+            const lastUserId = users[users.length - 1].id;
+            const newUser = {
+                id: lastUserId + 1,
+                name: body.name
+            }
+
+            users.push(newUser);
+
+            response.send(200, newUser);
+        })
+
+    },
 }
--- src/routes.js
+++ src/routes.js
@@ -15,5 +15,10 @@ module.exports = [
         endpoint: '/users/:id',
         method: 'GET',
         handler: UserController.getUserById,
+    },
+    {
+        endpoint: '/users',
+        method: 'POST',
+        handler: UserController.createUser,
     }
 ]
